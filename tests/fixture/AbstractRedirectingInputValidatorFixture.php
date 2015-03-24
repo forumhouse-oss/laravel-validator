d58ffde5b250948e547c4c2d43443814b2714c97
Vladislav Rastrusny <FractalizeR@yandex.ru>
2015-03-24 14:26:55 +0300

    Test\Fixture\AbstractInputValidatorFixture cleanup

--- tests/fixture/AbstractRedirectingInputValidatorFixture.php
+++ tests/fixture/AbstractRedirectingInputValidatorFixture.php
@@ -11,7 +11,7 @@ use FHTeam\LaravelValidator\Validator\Input\AbstractInputValidator;
  * @mixin ArrayDataStorage
  * @package FHTeam\LaravelValidator\Test\Fixture
  */
-class AbstractInputValidatorFixture extends AbstractInputValidator
+class AbstractRedirectingInputValidatorFixture extends AbstractInputValidator
 {
     protected $rules = [
         'unused' => ['dummy' => 'required'],
@@ -21,6 +21,23 @@ class AbstractInputValidatorFixture extends AbstractInputValidator
         ],
     ];
 
+    protected $errorRedirects = [
+        'simple_route' => [
+            'route' => 'test_route',
+        ],
+        'simple_route_array' => [
+            'route' => ['test_route', ['param1' => 'value1']],
+        ],
+        'simple_action' => [
+            'action' => 'test_route',
+        ]
+    ];
+
+    /**
+     * @var string
+     */
+    protected $currentRouteMethod;
+
     /**
      * @param $object
      *
@@ -29,5 +46,13 @@ class AbstractInputValidatorFixture extends AbstractInputValidator
     protected function getObjectData($object = null)
     {
         return $object;
+    }
+
+    /**
+     * @param string $currentRouteMethod
+     */
+    public function setCurrentRouteMethod($currentRouteMethod)
+    {
+        $this->currentRouteMethod = $currentRouteMethod;
     }
 }
