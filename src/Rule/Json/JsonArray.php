758bce4c22869d905eae25ff771a9f7188ab9a17
Vladislav Rastrusny <FractalizeR@yandex.ru>
2015-05-07 19:52:38 +0300

    Rule\Json\JsonArray introduced

---
+++ src/Rule/Json/JsonArray.php
@@ -0,0 +1,37 @@
+<?php namespace FHTeam\LaravelValidator\Rule\Json;
+
+use FHTeam\LaravelValidator\Rule\AbstractValidationRule;
+use FHTeam\LaravelValidator\Rule\ValidationRuleInterface;
+
+/**
+ * Validation rule to test if a given JSON string contains array.
+ * Minimum and maximum valid sizes for array can be specified by first and second optional parameters
+ *
+ * @package FHTeam\LaravelValidator\Rule
+ */
+class JsonArray extends AbstractValidationRule implements ValidationRuleInterface
+{
+    public function validate($attribute, $value, array $parameters = [])
+    {
+        $result = json_decode($value, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return false;
+        }
+
+        if (!is_array($result)) {
+            return false;
+        }
+
+        $arrayLength = count($result);
+
+        if (isset($parameters[0]) && $arrayLength < $parameters[0]) {
+            return false;
+        }
+
+        if (isset($parameters[1]) && $arrayLength > $parameters[1]) {
+            return false;
+        }
+
+        return true;
+    }
+}
