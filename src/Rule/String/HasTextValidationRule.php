d93844a8efe0e225001cff67523a949345ef12ec
Vladislav Rastrusny <FractalizeR@yandex.ru>
2015-06-26 17:11:12 +0300

    Two text validation rules added

---
+++ src/Rule/String/HasTextValidationRule.php
@@ -0,0 +1,17 @@
+<?php namespace FHTeam\LaravelValidator\Rule\Json;
+
+use FHTeam\LaravelValidator\Rule\AbstractValidationRule;
+use FHTeam\LaravelValidator\Rule\ValidationRuleInterface;
+
+/**
+ * Validation rule to test if a given string is not empty (does not consist of spaces, for instance)
+ *
+ * @package FHTeam\LaravelValidator\Rule
+ */
+class TextNotEmptyStringValidationRule extends AbstractValidationRule implements ValidationRuleInterface
+{
+    public function validate($attribute, $value, array $parameters = [])
+    {
+        return mb_strlen(trim($value)) > 0;
+    }
+}
---
+++ src/Rule/String/TextSize.php
@@ -0,0 +1,31 @@
+<?php namespace FHTeam\LaravelValidator\Rule\Json;
+
+use Exception;
+use FHTeam\LaravelValidator\Rule\AbstractValidationRule;
+use FHTeam\LaravelValidator\Rule\ValidationRuleInterface;
+
+/**
+ * Validation rule to test given string's size using Unicode text functions
+ * Minimum and maximum valid sizes for array can be specified by first and second optional parameters
+ *
+ * @package FHTeam\LaravelValidator\Rule
+ */
+class TextSizeValidationRule extends AbstractValidationRule implements ValidationRuleInterface
+{
+    public function validate($attribute, $value, array $parameters = [])
+    {
+        if (count($parameters) < 1) {
+            throw new Exception("Text Size validation rule requires at least one parameter");
+        }
+
+        if (mb_strlen($value) < $parameters[0]) {
+            return false;
+        }
+
+        if (isset($parameters[1]) && mb_strlen($value) > $parameters[1]) {
+            return false;
+        }
+
+        return true;
+    }
+}
