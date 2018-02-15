a185aa28a3c3cc89e759775b770348c284f43f37
god1dog <god6dog@gmail.com>
2018-02-15 13:39:07 +0300

    Fix bug for CLI

--- src/Validator/AbstractValidator.php
+++ src/Validator/AbstractValidator.php
@@ -146,6 +146,9 @@ abstract class AbstractValidator implements MessageProvider, ArrayAccess, Iterat
      */
     public function isThisValid($object = null)
     {
+        if ('cli' === PHP_SAPI) {
+            return true;
+        }
         $objectData = $this->getObjectData($object);
         $this->lastState = $this->getState($object);
         $rules = Arr::mergeByCondition($this->rules, $this->lastState);
